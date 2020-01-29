67dbec2716801f9c9ca401c0045773014779e162
svvimming <benmunozj@gmail.com>
2020-01-29 11:53:25 -0500

    submit color

--- index.js
+++ index.js
@@ -6,19 +6,24 @@ var fs = require('fs');
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static('client'));
 app.use(cors());
 
-// viewed at http://localhost:8080
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/client/'));
 });
 
+app.get('/bichos', function(req, res){
+  var bichos = fs.readdirSync('./client/assets');
+  var path_name = bichos[bichos.length-1];
+  res.json({ path: 'assets/'+path_name });
+});
+
 app.post('/drawing', function(req, res) {
   var data = req.body;
-  fs.writeFile('client/assets/temp.json', JSON.stringify(data), (err) => {
-    // JSON.stringify(data)
+  var d = Date.now();
+  var path_name = 'client/assets/bicho_'+d+'.json';
+  fs.writeFile(path_name, JSON.stringify(data), (err) => {
     if (err) console.log(err);
     console.log("Successfully Written to File.");
   });
